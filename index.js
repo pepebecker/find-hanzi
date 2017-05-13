509302a873f45c7fd3a29c52b839b4518c388a5f
Pepe Becker <mail@pepebecker.com>
2017-05-13 22:03:07 +0200

    Update module to use multiple json files as database

--- index.js
+++ index.js
@@ -1,130 +1,60 @@
 'use strict'
 
-const cangjie = require('./cangjie.json')
+const cangjie2radicals	= require('./cangjie.json')
+const cangjie2hanzi			= require('./data/cangjie2hanzi.json')
+const hanzi2cangjie			= require('./data/hanzi2cangjie.json')
+const hanzi2definition	= require('./data/hanzi2definition.json')
+const hanzi2frequency		= require('./data/hanzi2frequency.json')
+const hanzi2pinyin			= require('./data/hanzi2pinyin.json')
+const hanzi2strokes			= require('./data/hanzi2strokes.json')
+const pinyin2hanzi			= require('./data/pinyin2hanzi.json')
+
 const utils = require('pinyin-utils')
-const isNode = require('detect-node')
-const readline = require('readline')
-const fs = require('fs')
-const path = require('path')
-
-let storedData
-
-let matches = []
-
-const onLine = (line, query, options, foundMatch) => {
-	const params = line.split('\t')
-
-	let item = {
-		unicode: params[0],
-		hanzi: params[1],
-		pinyin: params[2],
-		cangjie: params[3],
-		definition: params[4],
-		frequency: params[5]
-	}
-
-	if (options.multiple) {
-		for (let i in matches) {
-			if (matches[i] && matches[i].item === undefined) {
-				if (matches[i].input === item.hanzi) {
-					matches[i].item = item
-				}
-			}
-		}
-		return
-	}
-
-	if (options.search) {
-		if (item.definition.match(new RegExp('\\b(' + query + ')\\b'))) {
-			matches.push(item)
-		}
-	} else {
-		if (options.fuzzy) {
-			if (utils.removeTone(query) && utils.removeTone(query) === utils.removeTone(item.pinyin)) {
-				matches.push(item)
-			}
-		} else {
-			if (utils.numberToMark(query) === item.pinyin) {
-				matches.push(item)
-			}
-		}
-		if (query === item.cangjie || query === item.unicode || query === item.hanzi) {
-			matches = [item]
-			foundMatch()
-		}
-	}
-}
-
-const onDone = (options, yay, nay) => {
-	if (options.multiple) {
-		matches = matches.map((match) => match.item || match.input)
-		yay && yay(matches)
-		return
-	}
-	if (matches.length > 0) {
-		matches = matches.map((match) => {
-			let code = match.cangjie.split('')
-			code = code.map((c) => cangjie[c])
-			match.kangjie = code.join('')
-			match.frequency = match.frequency || 9
-			return match
-		})
-		matches = matches.sort((a, b) => a.frequency - b.frequency)
-		if (options && options.results) {
-			matches = matches.slice(0, options.results)
-		}
-	}
-	yay && yay(matches)
-	matches = []
-}
-
-const loadData = (done) => {
-	if (storedData) {
-		done && done(storedData)
-	} else {
-		const client = new XMLHttpRequest()
-		client.open('GET', '/data.txt')
-		client.onreadystatechange = function() {
-			storedData = client.responseText.split('\n')
-			console.log('Data sucessfully loaded')
-			loadData()
-		}
-		client.send()
-	}
-}
 
 const find = (query, options = {}) => new Promise((yay, nay) => {
-	if (options.multiple) {
-		matches = query.split('').map((input) => ({input}))
+	let cangjie = hanzi2cangjie[query]	// Input type: hanzi
+	let hanzi = cangjie2hanzi[query]		// Input type: cangjie
+	let hanzies = pinyin2hanzi[utils.numberToMark(query)] // Input type: pinyin
+
+	// Input is hanzi
+	if (cangjie) {
+		const hanzi = query
+		const pinyin = hanzi2pinyin[hanzi]
+		const cangjie2 = cangjie.split('').map((c) => cangjie2radicals[c]).join('')
+		const strokes = hanzi2strokes[hanzi]
+		const definition = hanzi2definition[hanzi]
+		yay([{hanzi, pinyin, cangjie, cangjie2, strokes, definition}])
+		return
 	}
 
-	if (isNode) {
-		const rl = readline.createInterface({
-			input: fs.createReadStream(path.join(__dirname, './data.txt'))
-		})
-
-		rl.on('line', function (line) {
-			onLine(line, query, options, () => rl.close())
-		})
-
-		rl.on('close', function () {
-			onDone(options, yay, nay)
-		})
-	} else {
-		loadData((data) => {
-			for (let line of data) {
-				onLine(line, query, options, () => {
-					onDone()
-					return
-				})
-			}
-			onDone(options, yay, nay)
-		})
+	// Input is cangjie
+	if (hanzi) {
+		cangjie = query
+		const pinyin = hanzi2pinyin[hanzi]
+		const cangjie2 = cangjie.split('').map((c) => cangjie2radicals[c]).join('')
+		const strokes = hanzi2strokes[hanzi]
+		const definition = hanzi2definition[hanzi]
+		yay([{hanzi, pinyin, cangjie, cangjie2, strokes, definition}])
+		return
 	}
+
+	// Input is pinyin
+	if (hanzies) {
+		const list = []
+		for (hanzi of hanzies) {
+			cangjie = hanzi2cangjie[hanzi]
+			if (!cangjie) continue
+			const pinyin = hanzi2pinyin[hanzi] || utils.numberToMark(query)
+			const cangjie2 = cangjie.split('').map((c) => cangjie2radicals[c]).join('')
+			const strokes = hanzi2strokes[hanzi]
+			const definition = hanzi2definition[hanzi]
+			list.push({hanzi, pinyin, cangjie, cangjie2, strokes, definition})
+		}
+		yay(list)
+		return
+	}
+	
+	nay('Unrecognized input type')
 })
-
-if (!isNode) {
-	loadData()
-}
 
 module.exports = find
--- test/index.js
+++ test/index.js
@@ -19,13 +19,3 @@ describe('Get pinyin from character', () => {
 		})
 	})
 })
-
-describe('Get multiple characters', () => {
-	it('should return list of characters', () => {
-		return findHanzi('我 的猫喜欢喝牛奶', {multiple: true}).then((data) => {
-			data = data.map((item) => item.pinyin || item)
-			const py = ['wǒ', ' ', 'de', 'māo', 'xǐ', 'huān', 'hē', 'niú', 'nǎi']
-			data.should.deepEqual(py)
-		})
-	})
-})
